d8158a07f8d7a381f711b7663a4d5e4d103fa8ff
amirreza <amirrezaazizi538@gmail.com>
2026-05-30 12:38:09 +0330

    delete backend file

--- backend/profiling/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import InterestCategory
-
-class InterestCategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = InterestCategory
-        fields = ['id', 'title', 'slug']
--- backend/profiling/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import SubmitInterestsView, InterestListView
-
-urlpatterns = [
-    path('interests/', InterestListView.as_view(), name='api-interest-list'),
-    path('submit-interests/', SubmitInterestsView.as_view(), name='api-submit-interests'),
-]
